fcd5462147df051907c157869d783ec95673a67f
Stephenfang51 <r851000@gmail.com>
2020-01-18 21:16:29 +0800

    update className

--- include/cttrt_config.h
+++ include/cttrt_config.h
@@ -15,8 +15,23 @@ namespace cttrt {
     static int kernelSize = 4 ;
 
 
-    //for center Net detection
-    static int classNum = 2 ;
+    //for COCO detection
+    static int classNum = 80 ;
+    constexpr static char *className[] = {
+            (char*)"person", (char*)"bicycle", (char*)"car", (char*)"motorcycle", (char*)"airplane",
+            (char*)"bus", (char*)"train", (char*)"truck", (char*)"boat", (char*)"traffic light", (char*)"fire hydrant",
+            (char*)"stop sign", (char*)"parking meter", (char*)"bench", (char*)"bird", (char*)"cat", (char*)"dog", (char*)"horse",
+            (char*)"sheep", (char*)"cow", (char*)"elephant", (char*)"bear", (char*)"zebra", (char*)"giraffe", (char*)"backpack",
+            (char*)"umbrella", (char*)"handbag", (char*)"tie", (char*)"suitcase", (char*)"frisbee", (char*)"skis",
+            (char*)"snowboard", (char*)"sports ball", (char*)"kite", (char*)"baseball bat", (char*)"baseball glove",
+            (char*)"skateboard", (char*)"surfboard", (char*)"tennis racket", (char*)"bottle", (char*)"wine glass",
+            (char*)"cup", (char*)"fork", (char*)"knife", (char*)"spoon", (char*)"bowl", (char*)"banana", (char*)"apple", (char*)"sandwich",
+            (char*)"orange", (char*)"broccoli", (char*)"carrot", (char*)"hot dog", (char*)"pizza", (char*)"donut", (char*)"cake",
+            (char*)"chair", (char*)"couch", (char*)"potted plant", (char*)"bed", (char*)"dining table", (char*)"toilet", (char*)"tv",
+            (char*)"laptop", (char*)"mouse", (char*)"remote", (char*)"keyboard", (char*)"cell phone", (char*)"microwave",
+            (char*)"oven", (char*)"toaster", (char*)"sink", (char*)"refrigerator", (char*)"book", (char*)"clock", (char*)"vase",
+            (char*)"scissors", (char*)"teddy bear", (char*)"hair drier", (char*)"toothbrush"
+    };
  
 //    static char *className[]= {(char*)"person",(char*)"helmet"};
 }
